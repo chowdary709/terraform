7ec3ed77879230fc33e73a5aadb34a57354f7102
chowdary709 <chandrahari321@gmail.com>
2024-05-20 13:26:17 +0530

    update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,25 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name  = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets[count.index]
+  azs = var.azs[count.index]
+
+  tags = {
+    Name = "private_subnets-${count.index + 1}"
+  }
+}
