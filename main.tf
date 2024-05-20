4a2c550c1f4838a487d194b776d01eb8503fb4f7
chowdary709 <chandrahari321@gmail.com>
2024-05-20 17:04:04 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-  source = "./modules/vpc"
-  env = var.env
-  vpc_cidr = var.vpc_cidr
-  azs = var.azs
-  private_subnets = var.private_subnets
-  public_subnets = var.public_subnets
-  account_number = var.account_number
-  default_vpc_id = var.default_vpc_id
-  default_vpc_cidr = var.default_vpc_cidr
+  source                 = "./modules/vpc"
+  env                    = var.env
+  vpc_cidr               = var.vpc_cidr
+  azs                    = var.azs
+  private_subnets        = var.private_subnets
+  public_subnets         = var.public_subnets
+  account_number         = var.account_number
+  default_vpc_id         = var.default_vpc_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
