89db28989e019061cf4a03e81ed63a3424a8eec7
chowdary709 <chandrahari321@gmail.com>
2024-05-22 14:29:32 +0530

    update

---
+++ modules/app/iam_role.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "test_role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action"    : "sts:AssumeRole",
+        "Effect"    : "Allow",
+        "Sid"       : ""
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameters",
+            "ssm:GetParametersByPath",
+            "ssm:DescribeParameters"
+          ],
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
