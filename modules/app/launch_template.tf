2f11e82ec5c7cdc675d4a4aa31ad7a7dc22e51d8
chowdary709 <chandrahari321@gmail.com>
2024-05-22 11:48:38 +0530

    update

--- modules/app/launch_template.tf
+++ modules/app/launch_template.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = aws_security_group.sg.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tag_specifications {
     resource_type = "instance"
