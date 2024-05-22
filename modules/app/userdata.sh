c25ce97543eb3c432060fe46873077b8188c3fe3
chowdary709 <chandrahari321@gmail.com>
2024-05-22 13:03:44 +0530

    update

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dnf install python3.11-pip -y
-pip3.11 install boto3 botocore
-ansible-pull -i localhost, https://github.com/chowdary709/infra-ansible main.yml -e role_name=${role_name}
+dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
+pip3.11 install boto3 botocore | tee -a /opt/userdata.log
+ansible-pull -i localhost, https://github.com/chowdary709/infra-ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
 
 
 
