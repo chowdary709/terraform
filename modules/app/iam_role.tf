86d0c44c5f37b0cd3ca3b7242c404f3d4e079139
chowdary709 <chandrahari321@gmail.com>
2024-05-22 14:47:59 +0530

    update

--- modules/app/iam_role.tf
+++ modules/app/iam_role.tf
@@ -2,34 +2,36 @@ resource "aws_iam_role" "role" {
   name = "${var.env}-${var.component}-role"
 
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Action"    : "sts:AssumeRole",
-        "Effect"    : "Allow",
-        "Sid"       : ""
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
-      }
+      },
     ]
   })
 
   inline_policy {
-    name = "${var.env}-${var.component}"
+    name = "${var.env}-${var.component}-policy"
 
     policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-          "Sid": "VisualEditor0",
-          "Effect": "Allow",
-          "Action": [
-            "ssm:GetParameters",
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
             "ssm:GetParametersByPath",
-            "ssm:DescribeParameters"
+            "ssm:GetParameters",
+            "ssm:GetParameter"
           ],
-          "Resource": "*"
+          "Resource" : "*"
         }
       ]
     })
@@ -39,7 +41,3 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.env}-${var.component}-role"
   }
 }
-# resource "aws_iam_instance_profile" "instance_profile" {
-#   name = "${var.env}-${var.component}-role"
-#   role = aws_iam_role.role.name
-# }
