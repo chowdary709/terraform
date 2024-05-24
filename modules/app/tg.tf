61a149e1f591d4ac614ebb42be0574cca099fee5
chowdary709 <chandrahari321@gmail.com>
2024-05-24 18:26:06 +0530

    update

--- modules/app/tg.tf
+++ modules/app/tg.tf
@@ -4,10 +4,13 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check {
-    healthy_threshold   = 5
-    unhealthy_threshold = 1
-    timeout             = 5
-    Interval            = 5
-    path                = "./health"
+    enabled             = true             # Enable health checks for the application
+    healthy_threshold   = 2                # Number of consecutive successful health checks before considering the instance healthy
+    unhealthy_threshold = 2                # Number of consecutive failed health checks before considering the instance unhealthy
+    timeout             = 3                # Time (in seconds) to wait for a health check response before considering it a failure
+    port                = var.app_port     # Port on which the health check requests will be sent (using a variable)
+    interval            = 5                # Time (in seconds) between each health check
+    path                = "./health"       # Path to the health check endpoint on the application
   }
+
 }
