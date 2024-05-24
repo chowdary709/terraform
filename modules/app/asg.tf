55a15f5a5526e35fba2403e5ac4fa17fc581c903
chowdary709 <chandrahari321@gmail.com>
2024-05-24 17:51:49 +0530

    update

--- modules/app/asg.tf
+++ modules/app/asg.tf
@@ -5,6 +5,7 @@ resource "aws_autoscaling_group" "asg" {
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
   force_delete        = true
+  target_group_arns   = [aws_lb_target_group.tg.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
---
+++ modules/app/tg.tf
@@ -0,0 +1,6 @@
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.env}-${var.component}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
