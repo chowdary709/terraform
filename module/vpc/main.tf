be752166b9a8e54c8c8d84e50b73deb551621c06
chowdary709 <chandrahari321@gmail.com>
2024-06-25 10:54:30 +0530

    automated commit #2847

---
+++ module/vpc/main.tf
@@ -0,0 +1,70 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "${var.env}-public-subnets"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "${var.env}-private-subnets"
+  }
+}
+
+resource "aws_vpc_peering_connection" "peering_connection" {
+  peer_vpc_id = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
+  auto_accept = true
+  tags = {
+    Name = "peering-from-default-vpc-${var.env}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+# resource "aws_eip" "eip" {
+#   domain   = "vpc"
+#   tags = {
+#     Name = "${var.env}-eip"
+#   }
+# }
+#
+# resource "aws_nat_gateway" "ngw" {
+#   allocation_id = aws_eip.eip.id
+#   subnet_id = aws_subnet.public[0].id
+#   tags = {
+#     Name = "${var.env}-ngw"
+#   }
+# }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.env}-public"
+  }
+}
