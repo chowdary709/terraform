d8e4df67433127ddb384f058b1a2473d054d22d2
chowdary709 <chandrahari321@gmail.com>
2024-06-25 11:44:28 +0530

    automated commit #2850

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -68,3 +68,19 @@ resource "aws_route_table" "public" {
     Name = "${var.env}-public"
   }
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.ngw.id
+#   }
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
+  }
+  tags = {
+    Name = "${var.env}-private"
+  }
+}
