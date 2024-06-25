b75b668e0b3ce64d64da064397f04642d06279dd
chowdary709 <chandrahari321@gmail.com>
2024-06-25 11:54:19 +0530

    automated commit #2852

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -98,6 +98,6 @@ resource "aws_route_table_association" "public" {
 }
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnets)
-  subnet_id = aws_subnet.private.id
+  subnet_id = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
